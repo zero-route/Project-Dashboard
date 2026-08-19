62ab7d505996831740730efd42c1c8779cc28cb8
zero-route <iostream911@gmail.com>
2026-08-20 01:51:25 +0800

    update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1526,6 +1526,12 @@ function VercelProject() {
         })}
       </div>
 
+              )}
+            </Card>
+          );
+        })}
+      </div>
+
       <style>{`
         @keyframes ping {
           75%, 100% { transform: scale(2); opacity: 0; }
